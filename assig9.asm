6091d5cfe7a40eb47e01632409ec588b2b221919
Deepa <169141996+DEEPAcs034@users.noreply.github.com>
2025-04-08 19:33:44 +0530

    Update assig9.asm

--- assig9.asm
+++ assig9.asm
@@ -2,7 +2,7 @@
 ;the registers  r0, r1 and r2 contain the values 0x00000000, 0x11112222 and  0X40000000 respectively,
 ;what will be the value in register r0, r1 and r2  after execution of the following instruction 
 ;SWP r0, r1, [r2]
-  AREA PRG9, CODE, READONLY  ; Define a code section named SWAP, marked as read-only
+  AREA QUESTION9, CODE, READONLY  ; Define a code section named QUESTION9, marked as read-only
 START
   LDR R0,=0x00000000  ; Load immediate value 0x00000000 into register R0
   LDR R1,=0x11112222  ; Load immediate value 0x11112222 into register R1
